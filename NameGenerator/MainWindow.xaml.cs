53542300316d1cf41de2a7ff07a7e5c3c311ede9
brabideau1 <brabideau1@studentmail.nait.ca>
2017-09-09 17:40:21 -0600

    Breaks with Korean names. Dropdowns not yet functional.

--- NameGenerator/MainWindow.xaml.cs
+++ NameGenerator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,14 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace NameGenerator
 {
@@ -23,6 +17,8 @@ namespace NameGenerator
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        #region properties and fields
         public enum Gender { Male, Female, Either }
 
         public enum NameType { Given, Surname, Both }
@@ -34,12 +30,21 @@ namespace NameGenerator
         public Gender ChosenGender { get; set; }
         public NameType ChosenNameType { get; set; }
 
-        private List<string> SourceNames { get; set; }
+        private Random _random;
+        private List<string> _sourceNames { get; set; }
         public List<string> Generated { get; set; }
         //public List<Origin> Origins { get; set; }
         public List<NameOrigin> Places { get; set; }
         public int Order { get; set; }
+        private int _minLength;
+        private int _maxLength;
         public int OutputCount { get; set; }
+
+        private Dictionary<string, Dictionary<string, int>> _frequencies;
+
+        private Dictionary<string, int> _firstChars;/* { get; set; }*/
+        #endregion
+
 
         public MainWindow()
         {
@@ -49,7 +54,7 @@ namespace NameGenerator
             Order = 3;
             OutputCount = 20;
 
-            SourceNames = new List<string>();
+            _sourceNames = new List<string>();
             Generated = new List<string>();
             Places = new List<NameOrigin>();
 
@@ -68,42 +73,57 @@ namespace NameGenerator
 
         private void Generate_Click(object sender, RoutedEventArgs e)
         {
-            //string sources = "Swedish-language_surnames";
+            List<string> totalSources = Places.Where(p => p.IsChecked).Select(p => p.Surnames).ToList();
+            _sourceNames = new List<string>();
 
-            string sources = string.Join("|", Places.Where(p => p.IsChecked).Select(p => p.Surnames));          
 
-            string query = String.Format("??action=query&generator=categorymembers&gcmlimit=500&format=json&gcmtitle=Category:{0}", sources);
-
-            HttpResponseMessage response = _client.GetAsync(query).Result;
-
-            var result = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
-
-            while (result["continue"] != null)
+            foreach(string source in totalSources)
             {
-                string continuequery = string.Format(query + "&gcmcontinue= {0}", result["continue"]["gcmcontinue"]);
-                var rawlist = result["query"]["categorymembers"];
+                string query = String.Format("?action=query&generator=categorymembers&gcmlimit=500&gcmtitle=Category:{0}&format=json", source);
+                HttpResponseMessage response = _client.GetAsync(query).Result;
 
-                foreach (var name in rawlist)
+                var result = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+
+                var rawlist = result["query"]["pages"];
+
+                //TODO: Fix this it is dumb
+                foreach (var page in rawlist)
                 {
-                    if ((int)name["ns"] == 0)
+                    foreach(var name in page)
                     {
-                        SourceNames.Add((((string)name["title"]).Split('(')[0]).Trim());
+                        _sourceNames.Add((((string)name["title"]).Split('(')[0]).Trim());
                     }
                 }
+
+
+                while (result["batchcomplete"] == null)
+                {
+                    string continuequery = string.Format(query + "&gcmcontinue= {0}", result["continue"]["gcmcontinue"]);
+                    rawlist = result["query"]["pages"];
+
+                    foreach (var page in rawlist)
+                    {
+                        foreach (var name in page)
+                        {
+                            _sourceNames.Add((((string)name["title"]).Split('(')[0]).Trim());
+                        }
+                    }
+                }
+
             }
-           
 
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("LIST OF"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("SURNAME"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("FAMILY"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("FAMILIES"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains(" NAME"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("CUSTOMS"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("DYNASTIES"));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains(" AND "));
-            SourceNames.RemoveAll(n => n.ToUpper().Contains("CLAN"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("LIST OF"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("SURNAME"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("FAMILY"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("FAMILIES"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains(" NAME"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("CUSTOMS"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("DYNASTIES"));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains(" AND "));
+            _sourceNames.RemoveAll(n => n.ToUpper().Contains("CLAN"));
+            _sourceNames.RemoveAll(n => n.Length <= 1);
 
-            Generate();
+            GenerateList();
             RaisePropertyChanged("Generated");
         }
 
@@ -120,7 +140,8 @@ namespace NameGenerator
             Places.Add(new NameOrigin
             {
                 PlaceName = "English",
-                Surnames = "English-language_surnames|Surnames_of_English_origin|English_toponymic_surnames",
+                Surnames = "English-language_surnames",
+                //Surnames = "English-language_surnames|Surnames_of_English_origin|English_toponymic_surnames",
                 U_Names = "English_unisex_given_names|English_given_names",
                 F_Names = "English_feminine_given_names",
                 M_Names = "English_masculine_given_names"
@@ -129,7 +150,8 @@ namespace NameGenerator
             Places.Add(new NameOrigin
             {
                 PlaceName = "German",
-                Surnames = "German-language_surnames|Surnames_of_German_origin",
+                Surnames = "German-language_surnames",
+                //Surnames = "German-language_surnames|Surnames_of_German_origin",
                 U_Names = "German_given_names",
                 F_Names = "German_feminine_given_names",
                 M_Names = "German_masculine_given_names"
@@ -147,7 +169,8 @@ namespace NameGenerator
             Places.Add(new NameOrigin
             {
                 PlaceName = "Indian",
-                Surnames = "Indian_family_names|Surnames_of_Indian_origin",
+                Surnames = "Indian_family_names",
+                //Surnames = "Indian_family_names|Surnames_of_Indian_origin",
                 U_Names = "Indian_unisex_given_names|Indian_given_names",
                 F_Names = "Indian_feminine_given_names",
                 M_Names = "Indian_masculine_given_names"
@@ -156,7 +179,8 @@ namespace NameGenerator
             Places.Add(new NameOrigin
             {
                 PlaceName = "Italian",
-                Surnames = "Italian-language_surnames|Surnames_of_Italian_origin",
+                Surnames = "Italian-language_surnames",
+                //Surnames = "Italian-language_surnames|Surnames_of_Italian_origin",
                 U_Names = "Italian_given_names",
                 F_Names = "Italian_feminine_given_names",
                 M_Names = "Italian_masculine_given_names"
@@ -175,7 +199,8 @@ namespace NameGenerator
             {
                 PlaceName = "Korean",
                 Surnames = "Korean-language_surnames",
-                U_Names = "Korean_unisex_given_names|Korean_given_names",
+                U_Names = "Korean_unisex_given_names",
+                //U_Names = "Korean_unisex_given_names|Korean_given_names",
                 F_Names = "Korean_feminine_given_names",
                 M_Names = "Korean_masculine_given_names"
             });
@@ -193,7 +218,8 @@ namespace NameGenerator
             {
                 PlaceName = "Turkish",
                 Surnames = "Turkish-language_surnames",
-                U_Names = "Turkish_unisex_given_names|Turkish_given_names",
+                U_Names = "Turkish_unisex_given_names",
+                //U_Names = "Turkish_unisex_given_names|Turkish_given_names",
                 F_Names = "Turkish_feminine_given_names",
                 M_Names = "Turkish_masculine_given_names"
             });
@@ -203,12 +229,11 @@ namespace NameGenerator
         }
 
 
-
         private Dictionary<string, Dictionary<string, int>> GetFrequencies()
         {
             Dictionary<string, Dictionary<string, int>> frequencies = new Dictionary<string, Dictionary<string, int>>();
 
-            foreach (string n in SourceNames)
+            foreach (string n in _sourceNames)
             {
                 for (int i = 0; i < n.Length - Order; i++)
                 {
@@ -234,117 +259,116 @@ namespace NameGenerator
         }
 
 
-        private void Generate()
+        private void GenerateList()
         {
+            Order = 3;
             Generated = new List<string>();
-            Dictionary<string, int> firstChars = new Dictionary<string, int>();
+            _firstChars = new Dictionary<string, int>();
 
-            foreach (string n in SourceNames)
+            _minLength = _sourceNames.Min(n => n.Length);
+            _maxLength = _sourceNames.Max(n => n.Length);
+
+            if(Order <= _minLength) { Order = _minLength; }
+
+            foreach (string n in _sourceNames)
             {
                 string letter = n.Substring(0, 1);
-                if (!firstChars.ContainsKey(letter))
+                if (!_firstChars.ContainsKey(letter))
                 {
-                    firstChars.Add(letter, 1);
+                    _firstChars.Add(letter, 1);
                 }
                 else
                 {
-                    firstChars[letter]++;
+                    _firstChars[letter]++;
                 }
             }
 
-            Dictionary<string, Dictionary<string, int>> frequencies = GetFrequencies();
+            _frequencies = GetFrequencies();
 
-            Random rnd = new Random();
+            _random = new Random();
 
-            //picks a starting string for each generated name
-            for (int i = 0; i < OutputCount; i++)
+
+            while(Generated.Count < OutputCount )
             {
-                int randint = rnd.Next(0, SourceNames.Count);
-
-                foreach (string s in firstChars.Keys)
-                {
-                    string name = "";
-                    if (randint < firstChars[s])
-                    {
-
-                        for (int j = 0; j < Order; j++)
-                        {
-                            int total = 0;
-                            foreach (string myKey in frequencies.Keys.Where(st => st.Substring(0, 1) == s))
-                            {
-                                foreach (var t in frequencies[myKey])
-                                {
-                                    total += t.Value;
-                                }
-                            }
-
-                            int randcont = rnd.Next(0, total);
-
-                            foreach (string starter in frequencies.Keys.Where(st => st.Substring(0, 1) == s))
-                            {
-                                int subtotal = 0;
-                                foreach (var t in frequencies[starter])
-                                {
-                                    subtotal += t.Value;
-                                }
-                                if (randcont < subtotal)
-                                {
-                                    name = starter;
-                                    break;
-                                }
-                                randcont -= subtotal;
-                            }
-                        }
-
-                        //Generated.Add(test);
-
-                            Generated.Add(name);
-                        
-                        break;
-                    }
-                    randint -= firstChars[s];
-                }
+                GenerateName();
             }
-
-
-            //the rest of the name?
-            for (int i = 0; i < Generated.Count; i++)
-            {
-                string name = Generated[i];
-                string currStr = name.Substring(name.Length - Order);
-
-
-                //TODO: find a better way of continuing the name if currStr is not in the dictionary
-                while (currStr != "  " && frequencies.ContainsKey(currStr) && name.Length < 50)
-                {
-                    int total = 0;
-                    foreach (var d in frequencies[currStr])
-                    {
-                        total += d.Value;
-                    }
-                    int randint = rnd.Next(0, total);
-
-                    foreach (var d in frequencies[currStr])
-                    {
-                        if (randint < total)
-                        {
-                            name += d.Key;
-                            break;
-                        }
-                        randint -= total;
-                    }
-
-                    currStr = name.Substring(name.Length - Order);
-                }
-                if (!Generated.Contains(name.Trim()))
-                {
-                    Generated.Add(name);
-            }
-        }
 
             Generated.Sort();
         }
 
+        private void GenerateName()
+        {
+            int randint = _random.Next(0, _sourceNames.Count);
+            string name = "";
+
+            foreach (string s in _firstChars.Keys)
+            {
+                if (randint < _firstChars[s])
+                {
+                    for (int j = 0; j < Order; j++)
+                    {
+                        int total = 0;
+                        foreach (string myKey in _frequencies.Keys.Where(st => st.Substring(0, 1) == s))
+                        {
+                            foreach (var t in _frequencies[myKey])
+                            {
+                                total += t.Value;
+                            }
+                        }
+
+                        int randcont = _random.Next(0, total);
+
+                        foreach (string starter in _frequencies.Keys.Where(st => st.Substring(0, 1) == s))
+                        {
+                            int subtotal = 0;
+                            foreach (var t in _frequencies[starter])
+                            {
+                                subtotal += t.Value;
+                            }
+                            if (randcont < subtotal)
+                            {
+                                name = starter;
+                                break;
+                            }
+                            randcont -= subtotal;
+                        }
+                    }
+                    break;
+                }
+                randint -= _firstChars[s];
+            }
+            
+
+
+            //the rest of the name?
+
+
+            string currStr = name.Substring(name.Length - Order);
+
+            while (_frequencies.ContainsKey(currStr) && name.Length < _maxLength)
+            {
+                int total = 0;
+                foreach (var d in _frequencies[currStr])
+                {
+                    total += d.Value;
+                }
+                randint = _random.Next(0, total);
+
+                foreach (var d in _frequencies[currStr])
+                {
+                    if (randint < total)
+                    {
+                        name += d.Key;
+                        break;
+                    }
+                    randint -= total;
+                }
+                currStr = name.Substring(name.Length - Order);
+            }
+
+            if(name.Length > _minLength && !Generated.Contains(name)) { Generated.Add(name); }
+            
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
